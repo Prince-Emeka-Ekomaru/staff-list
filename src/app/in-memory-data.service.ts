194226f850d27f730d20435a2ad9d76e25501b0d
Prince  Chukwuemeka Ekomaru <princeekomaru@gmail.com>
2020-06-11 15:19:43 +0100

    new features

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,24 +8,188 @@ import { Staff } from './staff/staff';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const staff = [
-      { id: 1, name: 'Ayo', role: 'Director' },
-      { id: 2, name: 'Idris', role: 'Director2' },
-      { id: 3, name: 'Jumoke', role:'HR Manager' },
-      { id: 4, name: 'Seun', role:'Technical Head' },
-      { id: 5, name: 'Kaka', role:'Project Manager' },
-      { id: 6, name: 'Kazeem', role:'Team Lead' },
-      { id: 7, name: 'Eddie',role: 'Design Wizard' },
-      { id: 8, name: 'Ife', role:'IOS Master' },
-      { id: 9, name: 'Lizzy', role:'Back-end Egnr' },
-      { id: 10, name: 'Simbo',role:'Data DOCTOR ' },
-      { id: 11, name: 'Dav', role:'PHP Lord' },
-      { id: 12, name: 'Chux', role:'Network Batman' },
-      { id: 13, name: 'Nero', role:'Java Lord' },
-      { id: 14, name: 'Immanuel',role:'Remote JS Lord' },
-      { id: 15, name: 'KK',role:'Content Team Manager' },
-      { id: 16, name: 'Ramadan',role:'Back-end Intern' },
-      { id: 17, name: 'Saidat',role:'Content team Lead' },
-      { id: 18, name: 'Prince',role:'Front-end Intern' }
+      {
+        id: 1,
+         name: 'Ayo',
+         role: 'Director',
+         comments: 'an adventurous entrepreneur ',
+         points:9,
+         status: 'ACTIVE',
+         phone: '07064943111',
+         email: 'ayo@clane.com'
+       },
+
+       {
+         id: 2,
+          name: 'Idris',
+          role: 'Co-Director',
+          comments: 'an aggresive Senior Developer ',
+          points:15,
+          status: 'INACTIVE',
+          phone: '07064943111',
+          email: 'idris@clane.com'
+        },
+
+        {
+         id: 3,
+          name: 'Jumoke',
+          role: 'Human Resourse Manager',
+          comments: 'an inveigling master',
+          points:20,
+          status: 'ACTIVE',
+          phone: '07064943111',
+          email: 'jumoke@clane.com'
+        },
+        {
+         id: 4,
+          name: 'Fisayo',
+          role: 'Product Manager',
+          comments: 'an ingenious Product Manager ',
+          points:15,
+          status: 'INACTIVE',
+          phone: '07064943111',
+          email: 'fisayo@clane.com'
+        },
+        {
+         id: 5,
+          name: 'Leke',
+          role: 'Accountant',
+          comments: 'a frugal finance officer ',
+          points:17,
+          status: 'ACTIVE',
+          phone: '07064943111',
+          email: 'leke@clane.com'
+        },
+        {
+         id: 6,
+          name: 'Koromone',
+          role: 'Brand and Communication Manager',
+          comments: 'a silver-tongued communicator ',
+          points:15,
+          status: 'ACTIVE',
+          phone: '07064943111',
+          email: 'kk@clane.com'
+        },
+        {
+         id: 7,
+          name: 'Seun',
+          role: 'Software Engineer',
+          comments: 'a slick Senior Developer ',
+          points:18,
+          status: 'INACTIVE',
+          phone: '07063961675',
+          email: 'om@clane.com'
+        },
+        {
+         id: 8,
+          name: 'Kaka',
+          role: 'Project/Product Manager',
+          comments: 'a quirky Manager',
+          points:19,
+          status: 'ACTIVE',
+          phone: '07064943111',
+          email: 'k.ordor@clane.com'
+        },
+        {
+         id: 9,
+          name: 'Kazeem',
+          role: 'Software Engineer',
+          comments: 'an aggresive Senior Developer ',
+          points:20,
+          status: 'INACTIVE',
+          phone: '07064943111',
+          email: 'kazeem@clane.com'
+        },
+        {
+         id: 10,
+          name: 'Endurance',
+          role: 'U.X Engineer',
+          comments: 'a Promethean designer ',
+          points:13,
+          status: 'INACTIVE',
+          phone: '07064943111',
+          email: 'ee@clane.com'
+        },
+        {
+         id: 11,
+          name: 'Ifeoluwa',
+          role: 'IOS Developer',
+          comments: 'a provident IOS Engineer',
+          points:16,
+          status: 'INACTIVE',
+          phone: '07064943111',
+          email: 'i.king@clane.com'
+        },
+        {
+         id: 12,
+          name: 'Elizabeth Abbah',
+          role: 'Software Engineer',
+          comments: 'a halcyon Software Engineer',
+          points:17,
+          status: 'ACTIVE',
+          phone: '07064943111',
+          email: 'e.abbah@clane.com'
+        },
+        {
+         id: 13,
+          name: 'Nero',
+          role: 'Software Engineer',
+          comments: 'a placid Software Engineer ',
+          points:15,
+          status: 'INACTIVE',
+          phone: '07064943111',
+          email: 'o.okiewhru@clane.com'
+        },
+        {
+         id: 14,
+          name: 'Saidat',
+          role: 'Content Writer',
+          comments: 'a writter with a lot of slapstick moments',
+          points:20,
+          status: 'ACTIVE',
+          phone: '07064943111',
+          email: 'si@clane.com'
+        },
+        {
+         id: 15,
+          name: 'Olasimbo',
+          role: 'Data Engineer',
+          comments: 'a geeky Engineer',
+          points:11,
+          status: 'INACTIVE',
+          phone: '07064943111',
+          email: 'ayo@clane.com'
+        },
+        {
+         id: 16,
+          name: 'Emmanuel',
+          role: 'Software Developer',
+          comments: 'an aggresive Senior Developer ',
+          points:14,
+          status: 'INACTIVE',
+          phone: '07064943111',
+          email: 'e.akinde@clane.com'
+        },
+        {
+         id: 17,
+          name: 'Ramadan',
+          role: 'Intern',
+          comments: 'an imminent Back-end Developer ',
+          points:10,
+          status: 'INACTIVE',
+          phone: '07064943111',
+          email: 'olayiwolaramadan27@gmail.com'
+        },
+        // {
+        //  id: 18,
+        //   name: 'Prince',
+        //   role: 'Intern',
+        //   comments: 'a neophyte in Mobile Web Development ',
+        //   points:09,
+        //   status: 'ACTIVE',
+        //   phone: '07064943111',
+        //   email: 'p.ekomaru@clane.com'
+        // }
 
     ];
     return {staff};
--- src/app/staff/staff.ts
+++ src/app/staff/staff.ts
@@ -2,4 +2,9 @@ export interface Staff {
   id: number;
   name: string;
   role: string;
+  comments: string;
+  points: any;
+  status:  'ACTIVE' | 'INACTIVE';
+  email: string;
+  phone: string;
 }
